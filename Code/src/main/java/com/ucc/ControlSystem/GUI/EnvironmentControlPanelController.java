69bf440cdf85e99d04bfc992ecba1120cf03e8c5
Veres Noémi <veres.noemi@yahoo.com>
2022-11-13 13:53:54 +0000

    Created GUI for Environment Simulation

---
+++ Code/src/main/java/com/ucc/ControlSystem/GUI/EnvironmentControlPanelController.java
@@ -0,0 +1,14 @@
+package com.ucc.ControlSystem.GUI;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class EnvironmentControlPanelController implements ChangeListener {
+
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+
+}
--- Code/src/main/java/com/ucc/ControlSystem/Main.java
+++ Code/src/main/java/com/ucc/ControlSystem/Main.java
@@ -1,22 +1,15 @@
 package com.ucc.ControlSystem;
 
-import com.ucc.ControlSystem.ControlSystem.JDBC.ConnectionFactory;
-import com.ucc.ControlSystem.SystemConfiguration.SystemConfigParameters;
-import com.ucc.ControlSystem.SystemConfiguration.SystemConfigurationReader;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import com.ucc.ControlSystem.GUI.EnvironmentControlPanel;
 
-import java.sql.Connection;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String jdbUrl = SystemConfigurationReader.getSystemConfigurationReader().readEnvironmentVariable(SystemConfigParameters.DATABASE_URL);
-        String username = SystemConfigurationReader.getSystemConfigurationReader().readEnvironmentVariable(SystemConfigParameters.DATABASE_USER);
-        String pass = SystemConfigurationReader.getSystemConfigurationReader().readEnvironmentVariable(SystemConfigParameters.DATABASE_PASS);
+        JFrame frame = new EnvironmentControlPanel("Vertical Farm Control Simulation");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
-        ConnectionFactory.createDbConnection(jdbUrl,username,pass);
 
-        System.out.printf("Connection made!");
     }
 }
