7426f5f4c26e62fdade52d2ba491adb060d4c6ad
JakabSarolta <79626264+JakabSarolta@users.noreply.github.com>
2022-11-18 21:40:26 +0000

    gui checks parameters

--- Code/src/main/java/com/ucc/ControlSystem/GUI/AdminControlPanel.java
+++ Code/src/main/java/com/ucc/ControlSystem/GUI/AdminControlPanel.java
@@ -1,5 +1,9 @@
 package com.ucc.ControlSystem.GUI;
 
+import com.ucc.ControlSystem.ControlSystem.InputParameters.InputParameterProcessor;
+import com.ucc.ControlSystem.ControlSystem.InputParameters.OtherParameters;
+import com.ucc.ControlSystem.SimulationEnvironment.EnvironmentDeviceTypes;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -17,6 +21,16 @@ public class AdminControlPanel extends JFrame{
     private final JPanel parameters = new JPanel();
     private final JTabbedPane tabbedPane = new JTabbedPane();
     private final JButton updateButton = new JButton("      UPDATE       ");
+
+    private JTextField growthTime = new JTextField();
+    private JTextField minAirTemp = new JTextField();
+    private JTextField maxAirTemp = new JTextField();
+    private JTextField balancingTempCheck = new JTextField();
+    private JTextField balanceTempCheck = new JTextField();
+    private JLabel currentTime = new JLabel("", JLabel.CENTER);
+    private JLabel currentTemp = new JLabel("", JLabel.CENTER);
+    private JLabel actuatorState = new JLabel("Off", JLabel.CENTER);
+    private JLabel currentState = new JLabel("", JLabel.CENTER);
 
     Font  f1  = new Font("Our font", Font.BOLD, 16);
     Font  f2  = new Font("Our font", Font.BOLD, 14);
@@ -42,11 +56,6 @@ public class AdminControlPanel extends JFrame{
         balanceTempCheckLabel.setFont(f2);
         JLabel balancingTempCheckLabel = new JLabel("Temperature check for balancing state:");
         balancingTempCheckLabel.setFont(f2);
-        JTextField growthTime = new JTextField();
-        JTextField minAirTemp = new JTextField();
-        JTextField maxAirTemp = new JTextField();
-        JTextField balancingTempCheck = new JTextField();
-        JTextField balanceTempCheck = new JTextField();
 
         card1.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
@@ -81,7 +90,28 @@ public class AdminControlPanel extends JFrame{
         updateButton.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                if (getGrowthTime() == 0){
+                    JOptionPane.showMessageDialog(null, "Introduce a correct growing time!!");
+                } else {
+                    if (getMinAirTemp() == 0) {
+                        JOptionPane.showMessageDialog(null, "Introduce a correct min temperature value!!");
+                    } else {
+                        if(getMaxAirTemp() == 0){
+                            JOptionPane.showMessageDialog(null, "Introduce a correct max temperature value!!");
+                        } else
+                            if (getBalanceTempCheck() == 0) {
+                                JOptionPane.showMessageDialog(null, "Introduce a correct balanced state temperature checking interval value!!");
+                            } else {
+                                if (getBalancingTempCheck() == 0) {
+                                    JOptionPane.showMessageDialog(null, "Introduce a correct balancing state temperature checking interval value!!");
+                                } else{
+                                    InputParameterProcessor.getInputParameterProcessor().updateEnvironmentPropertyParameter(getMinAirTemp(), getMaxAirTemp(), EnvironmentDeviceTypes.AIR_TEMPERATURE);
+                                    InputParameterProcessor.getInputParameterProcessor().updateMeasurementIntervalParameter(getBalanceTempCheck(), getBalancingTempCheck(), EnvironmentDeviceTypes.AIR_TEMPERATURE);
+                                    InputParameterProcessor.getInputParameterProcessor().updateOtherParameter(OtherParameters.GROWTH_TIME, getGrowthTime());
+                                }
+                        }
+                    }
+                }
             }
         });
         c.gridx = 0;
@@ -108,25 +138,21 @@ public class AdminControlPanel extends JFrame{
         JLabel currentTimeLabel = new JLabel("Current time in growing phase:");
         currentTimeLabel.setFont(f1);
         currentValuesPanel.add(currentTimeLabel);
-        JLabel currentTime = new JLabel("", JLabel.CENTER);
         currentTime.setFont(f1);
         currentValuesPanel.add(currentTime);
         JLabel currentTempLabel = new JLabel("Current Temperature:");
         currentTempLabel.setFont(f1);
         currentValuesPanel.add(currentTempLabel);
-        JLabel currentTemp = new JLabel("", JLabel.CENTER);
         currentTemp.setFont(f1);
         currentValuesPanel.add(currentTemp);
         JLabel actuatorStateLabel = new JLabel("Actuator state:");
         actuatorStateLabel.setFont(f1);
         currentValuesPanel.add(actuatorStateLabel);
-        JLabel actuatorState = new JLabel("Off", JLabel.CENTER);
         actuatorState.setFont(f1);
         currentValuesPanel.add(actuatorState);
         JLabel currentStateLabel = new JLabel("Current state of the system:");
         currentStateLabel.setFont(f1);
         currentValuesPanel.add(currentStateLabel);
-        JLabel currentState = new JLabel("", JLabel.CENTER);
         currentState.setFont(f1);
         currentValuesPanel.add(currentState);
         c2.gridy = 1;
@@ -141,5 +167,62 @@ public class AdminControlPanel extends JFrame{
         tabbedPane.addTab(REPORTSPANEL, card4);
         this.add(tabbedPane, BorderLayout.CENTER);
         this.setContentPane(this.tabbedPane);
+    }
+
+    public long getGrowthTime() {
+        try{
+            //convert days into secs
+            return Long.parseLong(growthTime.getText()) * 86400;
+        }catch(NumberFormatException | NullPointerException n){
+            return 0;
+        }
+    }
+
+    public double getMinAirTemp() {
+        try{
+            return Double.parseDouble(minAirTemp.getText());
+        }catch(NumberFormatException | NullPointerException n){
+            return 0;
+        }
+    }
+
+    public double getMaxAirTemp() {
+        try{
+            return Double.parseDouble(maxAirTemp.getText());
+        }catch(NumberFormatException | NullPointerException n){
+            return 0;
+        }
+    }
+
+    public int getBalancingTempCheck() {
+        try{
+            return Integer.parseInt(balancingTempCheck.getText()) * 60;
+        }catch(NumberFormatException | NullPointerException n){
+            return 0;
+        }
+    }
+
+    public int getBalanceTempCheck() {
+        try{
+            return Integer.parseInt(balanceTempCheck.getText()) * 60;
+        }catch(NumberFormatException | NullPointerException n) {
+            return 0;
+        }
+    }
+
+    public void setCurrentTime(int currentTime) {
+        this.currentTime.setText(String.valueOf(currentTime));
+    }
+
+    public void setCurrentTemp(double currentTemp) {
+        this.currentTemp.setText(String.valueOf(currentTemp));
+    }
+
+    public void setActuatorState(String actuatorState) {
+        this.actuatorState.setText(String.valueOf(actuatorState));
+    }
+
+    public void setCurrentState(String currentState) {
+        this.currentState.setText(String.valueOf(currentState));
     }
 }
